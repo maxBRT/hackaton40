6bac8b077d7746d4243a74ca0e7eb3ed385f3642
maxBRT <bourret74@gmail.com>
2026-01-14 18:51:39 -0500

    Added proxy configuration in `frontend` (Vite server), Axios setup for API requests, CORS in `backend`, and makefile for project workflow automation.
    # Conflicts:
    #	backend/src/index.ts
    #	backend/test.rest

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,34 +2,38 @@ import express from 'express';
 import {authMiddleware} from "./middleware/authMiddleware";
 import dotenv from "dotenv";
 import UserRoutes from "./routes/user.routes";
+import CoursesRoutes from "./routes/courses.routes";
 import ModulesRoutes from "./routes/modules.routes";
 import LessonsRoutes from "./routes/lessons.routes";
 import EnrollRoutes from "./routes/enroll.routes";
 import QuizRoutes from "./routes/quiz.routes";
 import forumThreadsRoutes from "./routes/forum.routes";
-import CoursesRoutes from "./routes/courses.routes";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}))
 // Auth
-app.use("api/auth", UserRoutes);
+app.use("/api/auth", UserRoutes);
 
 // API
 app.use("/api/courses", CoursesRoutes);
 app.use("/api/modules", ModulesRoutes);
 app.use("/api/lessons", LessonsRoutes);
-app.use("api/enroll", EnrollRoutes);
-app.use("api/quiz", QuizRoutes);
-app.use("api/forum-treads", authMiddleware, forumThreadsRoutes);
-app.use("api/courses", CoursesRoutes);
+app.use("/api/enroll", EnrollRoutes);
+app.use("/api/quiz", QuizRoutes);
+app.use("/api/forum-treads", authMiddleware, forumThreadsRoutes);
+app.use("/api/courses", CoursesRoutes);
 
-app.get('/', authMiddleware, (req, res) => {
-    console.log(req.user);
-    res.send('Hello World');
+app.get('/api/', (req, res) => {
+    res.json({ message: 'Hello World'});
 })
 
 app.listen(process.env.PORT || 3000, () => {
