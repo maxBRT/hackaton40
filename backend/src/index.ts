f5c96ef3032d6f86e50e54ca1bd119b8249a9070
maxBRT <bourret74@gmail.com>
2026-01-14 18:16:31 -0500

    Updated API route prefixes and removed duplicate route entries.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import {authMiddleware} from "./middleware/authMiddleware";
 import dotenv from "dotenv";
 import UserRoutes from "./routes/user.routes";
-import CoursesRoutes from "./routes/courses.routes";
 import ModulesRoutes from "./routes/modules.routes";
 import LessonsRoutes from "./routes/lessons.routes";
 import EnrollRoutes from "./routes/enroll.routes";
@@ -14,19 +13,19 @@ dotenv.config();
 
 const app = express();
 
-// Auth
 app.use(express.json());
-app.use("/auth", UserRoutes);
+
+// Auth
+app.use("api/auth", UserRoutes);
 
 // API
 app.use("/api/courses", CoursesRoutes);
 app.use("/api/modules", ModulesRoutes);
 app.use("/api/lessons", LessonsRoutes);
-
-app.use("/enroll", EnrollRoutes);
-app.use("/quiz", QuizRoutes);
-app.use("/forum-treads", authMiddleware, forumThreadsRoutes);
-app.use("/courses", CoursesRoutes);
+app.use("api/enroll", EnrollRoutes);
+app.use("api/quiz", QuizRoutes);
+app.use("api/forum-treads", authMiddleware, forumThreadsRoutes);
+app.use("api/courses", CoursesRoutes);
 
 app.get('/', authMiddleware, (req, res) => {
     console.log(req.user);
