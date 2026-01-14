dca5871b7bab1212921a35fabae07cb841869548
maxBRT <bourret74@gmail.com>
2026-01-14 10:49:42 -0500

    batman

---
+++ backend/src/database/prisma.ts
@@ -0,0 +1,18 @@
+// utils/prisma.ts
+import { PrismaNeon } from '@prisma/adapter-neon';
+import { PrismaClient } from "../generated/prisma/client";
+import dotenv from "dotenv";
+dotenv.config();
+
+// Créer le client Neon adapter
+const adapter = new PrismaNeon({
+    connectionString: process.env.DATABASE_URL,
+});
+
+// Créer le client Prisma avec l'adaptateur Neon
+const prisma = new PrismaClient({
+    adapter,
+    log: ['query', 'info', 'warn', 'error'],
+});
+
+export default prisma;
